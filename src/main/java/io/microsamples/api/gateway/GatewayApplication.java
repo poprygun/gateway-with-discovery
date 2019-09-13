98d957099efedf563a6852380adaa3ce79de3209
Alex Shumilov <ashumilov@pivotal.io>
2019-09-13 08:50:27 -0400

    Initial commit.

---
+++ src/main/java/io/microsamples/api/gateway/GatewayApplication.java
@@ -0,0 +1,94 @@
+package io.microsamples.api.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import reactor.core.publisher.Mono;
+
+
+@SpringBootApplication
+public class GatewayApplication {
+    @Value("${test.uri:http://httpbin.org}")
+    String uri;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+//                .route("path_route", r -> r.path("/get")
+//                        .uri(uri))
+                .route("simple_route", r -> r.host("**.abc.org").and().path("/anything/png")
+                        .filters(f -> f.addResponseHeader("X-TestHeader", "foobar"))
+                        .uri(uri)
+                )
+                .route("read_body_pred", r -> r.host("*.readbody.org")
+                        .and().readBody(String.class,
+                                s -> s.trim().equalsIgnoreCase("hi"))
+                        .filters(f -> f.addResponseHeader("X-TestHeader", "read_body_pred")
+                        ).uri(uri)
+                )
+                .route("rewrite_request_obj", r -> r.host("*.rewriterequestobj.org")
+                        .filters(f -> f.addResponseHeader("X-TestHeader", "rewrite_request")
+                                .modifyRequestBody(String.class, Hello.class, MediaType.APPLICATION_JSON_VALUE,
+                                        (exchange, s) -> {
+                                            return Mono.just(new Hello(s.toUpperCase()));
+                                        })
+                        ).uri(uri)
+                )
+                .route("rewrite_request_upper", r -> r.host("*.rewriterequestupper.org")
+                        .filters(f -> f.addResponseHeader("X-TestHeader", "rewrite_request_upper")
+                                .modifyRequestBody(String.class, String.class,
+                                        (exchange, s) -> {
+                                            return Mono.just(s.toUpperCase() + s.toUpperCase());
+                                        })
+                        ).uri(uri)
+                )
+                .build();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    static class Hello {
+        String message;
+
+        public Hello() {
+        }
+
+        public Hello(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
+
+class AddRequestHeaderGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+
+    @Override
+    public GatewayFilter apply(AbstractNameValueGatewayFilterFactory.NameValueConfig config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest().mutate()
+                    .header(config.getName(), config.getValue())
+                    .build();
+
+            return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+}
